fa4e18f1c78abee35987637a71846502e9995e2a
Lautaring <lauti_lxb@hotmail.com>
2024-06-01 11:28:45 -0300

    Carga automática de imágenes

--- src/main/AdminGUI.py
+++ src/main/AdminGUI.py
@@ -5,6 +5,8 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 from logic.Rol import Rol
 from logic.Factor import Factor
+import glob
+import os
 
 class AdminGUI:
     def __init__(self, master):
@@ -32,6 +34,20 @@ class AdminGUI:
         # Inicializar el diccionario para almacenar los factores
         self.factors_dict = {}
 
+#Obtener las rutas relativas de la imagen y guardarlas en una lista de paths para luego utilizarlas
+    """ # Obtener la lista de rutas de las imágenes
+        self.image_paths = self.get_image_paths()
+
+    def get_image_paths(self):
+        # Buscar todos los archivos que coincidan con el patrón 'figureX.png' en la subcarpeta resources
+        base_dir = os.path.dirname(os.path.abspath(__file__))  # Obtiene el directorio del script actual
+        resources_dir = os.path.join(base_dir, 'resources')  # Directorio de la subcarpeta resources
+        image_files = glob.glob(os.path.join(resources_dir, 'figure*.png'))
+
+        # Ordenar los archivos por nombre para asegurar el orden correcto
+        image_files.sort()
+
+        return image_files """
     def load_image(self):
         img_path = filedialog.askopenfilename()  # Esto me deja elegir archivos
         if img_path:  # Si se seleccionó algo entonces
@@ -43,15 +59,30 @@ class AdminGUI:
             else:
                 print(f"No se pudo cargar la imagen {img_path}")
 
+    #Carga de imagenes automática
+    """def load_image(self):
+        print("image_paths: ", self.image_paths)
+        if self.image_paths:
+            img_path = self.image_paths[0]  # Cargar la primera imagen de la lista
+            self.last_img_path = img_path
+            self.img = cv2.imread(img_path)
+            if self.img is not None:
+                self.show_image()
+                self.create_form()
+            else:
+                print(f"No se pudo cargar la imagen {img_path}")
+        else:
+            print("No se encontraron imágenes.")"""
+
     def show_image(self):
         img_rgb = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
         img_pil = Image.fromarray(img_rgb)
 
-        # aca obtengo las dimensiones de la window
+        # obtener las dimensiones de la window
         window_width = self.master.winfo_width()
         window_height = self.master.winfo_height()
 
-        # estas son las dimensiones originales de la imagen
+        # Obtener las dimensiones originales de la imagen
         original_width, original_height = img_pil.size
 
         # factor de escala
@@ -64,13 +95,13 @@ class AdminGUI:
         new_height = int(original_height * scale_factor)
         img_pil_resized = img_pil.resize((new_width, new_height), Image.LANCZOS)
 
-        # ajustamos el lienzo con respecto a la venta (verlo bien)
+        # ajustamos el lienzo con respecto a la ventana (verlo bien)
         self.canvas.config(width=window_width, height=window_height)
 
         img_tk = ImageTk.PhotoImage(image=img_pil_resized)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=img_tk)
         self.canvas.image = img_tk
-        
+
     def create_form(self):
         tk.Label(self.form_frame, text="Nombre del Factor:").grid(row=0, column=0)
         self.factor_name_entry = tk.Entry(self.form_frame)
@@ -122,15 +153,25 @@ class AdminGUI:
             self.factors_dict[img_path] = factor
             self.clear_canvas()
             self.clear_form()
+
+            # Cargar la siguiente imagen y mostrarla
+            self.load_next_image()
+
+    #Sirve para la carga de imagenes automática
+    """def load_next_image(self):
+        current_index = len(self.factors_dict)  # Índice de la imagen actual
+        if current_index < len(self.image_paths):
+            next_img_path = self.image_paths[current_index]
+            self.last_img_path = next_img_path
+            self.img = cv2.imread(next_img_path)
+            if self.img is not None:
+                self.show_image()
+                self.create_form()
+            else:
+                print(f"No se pudo cargar la imagen {next_img_path}")
         else:
-            # Si no se ha cargado una imagen previamente, solicitar al usuario que seleccione una
-            img_path = filedialog.askopenfilename()
-            if img_path:  # Si el usuario selecciona un archivo
-                # Me guardo el factor junto con la imagen en el diccionario
-                self.factors_dict[img_path] = factor
-                self.clear_canvas()
-                self.clear_form()
-        
+            print("No hay más imágenes para cargar.") """
+
     def clear_form(self):
         # Borra los valores de todos los campos del formulario
         self.factor_name_entry.delete(0, tk.END)
